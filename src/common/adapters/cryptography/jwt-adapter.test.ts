f4f08f2d169a346d0ec742acd219547a6be967fa
guilhermelr291 <guilhermelr291dev@gmail.com>
2025-10-22 17:20:35 -0300

    test: add test suites to jwt-adapter

--- src/common/adapters/cryptography/jwt-adapter.test.ts
+++ src/common/adapters/cryptography/jwt-adapter.test.ts
@@ -6,6 +6,8 @@ import { JwtAdapter } from './jwt-adapter';
 vi.mock('jsonwebtoken', () => ({
   default: {
     sign: vi.fn().mockReturnValue('encoded_value'),
+    verify: vi.fn().mockReturnValue({ decoded: 'any_value' }),
+    decode: vi.fn().mockReturnValue({ decoded: 'any_value' }),
   },
 }));
 
@@ -40,6 +42,52 @@ describe('JwtAdapter', () => {
       });
 
       expect(() => sut.encrypt({ field: 'any_value' })).toThrow();
+    });
+  });
+
+  describe('decode', async () => {
+    test('Should call jwt.decode with correct value', () => {
+      const decodeSpy = vi.spyOn(jwt, 'decode');
+      sut.decode('any_token');
+
+      expect(decodeSpy).toHaveBeenCalledWith('any_token');
+    });
+
+    test('Should return decoded value on success', () => {
+      const result = sut.decode('any_token');
+      expect(result).toBeDefined();
+    });
+
+    test('Should throw if jwt.decode throws', () => {
+      vi.spyOn(jwt, 'decode').mockImplementationOnce(() => {
+        throw new Error();
+      });
+      expect(() => sut.decode('any_token')).toThrow();
+    });
+  });
+
+  describe('decode', async () => {
+    test('Should call jwt.verify with correct value', () => {
+      const verifySpy = vi.spyOn(jwt, 'verify');
+      sut.decrypt('any_token');
+
+      expect(verifySpy).toHaveBeenCalledWith(
+        'any_token',
+        process.env.JWT_SECRET
+      );
+    });
+
+    test('Should return decoded value on success', () => {
+      const result = sut.decrypt('any_token');
+      expect(result).toBeDefined();
+    });
+
+    test('Should throw if jwt.verify throws', () => {
+      vi.spyOn(jwt, 'verify').mockImplementationOnce(() => {
+        throw new Error();
+      });
+
+      expect(() => sut.decrypt('any_token')).toThrow();
     });
   });
 });
